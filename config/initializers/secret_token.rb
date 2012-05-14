b2ef9a604b2de568f7bd64ae7c6910769d3b45a4
FUKUI Osamu <ofukui@irubysystems.com>
2012-05-14 16:17:22 +0900

    maitore github化開始

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Maitore::Application.config.secret_token = '73bc9b5e3dc4c87eda505c9a9b7748644672ad7f97e19037861087a71a4fd9aad845098b0224261a15629d6cffd76264a768658e69c0d3ee69afd3cfc7b2f2af'
