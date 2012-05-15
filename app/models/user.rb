ea32736101630134072cd98412287cea0d995a9e
FUKUI Osamu <ofukui@irubysystems.com>
2012-05-16 06:54:44 +0900

    add mail_address validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
+class MailAddressValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors[attribute] << (options[:message] || "is not an email") unless
+      value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  end
+end
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
+  include ActiveModel::Validations
+
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
